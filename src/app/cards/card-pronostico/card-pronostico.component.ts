3107da80ecc714f1f5f494a9b176814c8a7181a3
Danirmd22 <daniramosm.06@gmail.com>
2024-05-02 20:08:06 +0200

    Añadir cambios en card-pronostico.component.html y card-pronostico.component.ts

--- src/app/cards/card-pronostico/card-pronostico.component.ts
+++ src/app/cards/card-pronostico/card-pronostico.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-card-pronostico',
   templateUrl: './card-pronostico.component.html',
@@ -9,5 +10,18 @@ import { faDroplet } from '@fortawesome/free-solid-svg-icons';
 export class CardPronosticoComponent {
   @Input() weatherData: any;
   @Input() forecastData: any;
+  nextForecasts: any[] = [];
   faDroplet = faDroplet;
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['forecastData'] && changes['forecastData'].currentValue) {
+      this.forecastData = changes['forecastData'].currentValue;
+      const now = new Date();
+      this.nextForecasts = this.forecastData.list.filter((forecast: any) => new Date(forecast.dt_txt) > now).slice(0, 4);
+    }
+  }
 }
+
+
+
