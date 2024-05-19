7432013c0a30711ffa59032308510b20ca4cc239
Daniel <daniramosm.06@gmail.com>
2024-05-19 17:17:09 +0200

    chore: Actualizar componente de mapa para agregar capas de visualización

--- src/app/shared/components/mapa/mapa.component.ts
+++ src/app/shared/components/mapa/mapa.component.ts
@@ -8,16 +8,59 @@ import * as L from 'leaflet';
 })
 export class MapaComponent implements AfterViewInit {
   private map!: L.Map;
+  private currentLayer: L.TileLayer | null = null;
+
+  constructor() { }
 
   ngAfterViewInit(): void {
     this.initMap();
   }
 
   private initMap(): void {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map');
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.map.setView([position.coords.latitude, position.coords.longitude], 13);
+    });
+
+    this.setLayer('standard');
+  }
+
+  public setLayer(layer: string): void {
+    let url: string;
+
+    switch (layer) {
+      case 'temperature':
+        url = 'https://{s}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png';
+        break;
+      case 'precipitation':
+        url = 'https://{s}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png';
+        break;
+      case 'clouds':
+        url = 'https://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png';
+        break;
+      default:
+        url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        break;
+    }
+
+    // Remueve la capa existente antes de agregar la nueva
+    if (this.currentLayer) {
+      this.map.removeLayer(this.currentLayer);
+    }
+
+    const newLayer = L.tileLayer(url, {
       maxZoom: 19,
-    }).addTo(this.map);
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.openweathermap.org/">OpenWeatherMap</a>',
+      id: 'openweathermap.' + layer,
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: '9e12ce681891bbfbbfce1e15fbad0f67'
+    });
+
+    newLayer.addTo(this.map);
+    this.currentLayer = newLayer;
   }
 }
